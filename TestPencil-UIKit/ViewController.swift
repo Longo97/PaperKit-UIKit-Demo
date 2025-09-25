abbe56c61b73708258093e2c3ec1e8e5e97f1d97
Longo97 <marco.longobardi997@gmail.com>
2025-09-25 16:35:36 +0200

    PaperKit Demo done

--- TestPencil-UIKit/ViewController.swift
+++ TestPencil-UIKit/ViewController.swift
@@ -6,14 +6,59 @@
 //
 
 import UIKit
+import PaperKit
+import PencilKit
 
 class ViewController: UIViewController {
-
+    
+    var paperVC: PaperMarkupViewController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        var featureSet = FeatureSet.latest
+        featureSet.colorMaximumLinearExposure = 4
+        
+        let markupModel = PaperMarkup(bounds: view.bounds)
+        paperVC = PaperMarkupViewController(markup: markupModel, supportedFeatureSet: featureSet)
+        
+        
+        guard let paperVC = paperVC else { return }
+        view.addSubview(paperVC.view)
+        addChild(paperVC)
+        paperVC.didMove(toParent: self)
+        
+        print("Done configuration")
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        guard let paperVC = paperVC else { return }
 
-
+        let toolpicker = PKToolPicker()
+        toolpicker.colorMaximumLinearExposure = 4
+        toolpicker.setVisible(true, forFirstResponder: paperVC)
+        toolpicker.addObserver(paperVC)
+        paperVC.pencilKitResponderState.activeToolPicker = toolpicker
+        paperVC.pencilKitResponderState.toolPickerVisibility = .visible
+        paperVC.becomeFirstResponder()
+        
+        toolpicker.accessoryItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(plusButtonPressed(_:)))
+    }
+    
+    @objc func plusButtonPressed(_ button: UIBarButtonItem) {
+        guard let paperVC = paperVC else { return }
+        var featureSet = FeatureSet.latest
+        featureSet.colorMaximumLinearExposure = 4
+        
+        let markupEditViewController = MarkupEditViewController(supportedFeatureSet: featureSet)
+        
+        markupEditViewController.delegate = paperVC as? any MarkupEditViewController.Delegate
+        markupEditViewController.modalPresentationStyle = .popover
+        markupEditViewController.popoverPresentationController?.barButtonItem = button
+        
+        present(markupEditViewController, animated: true)
+        
+    }
 }
 
